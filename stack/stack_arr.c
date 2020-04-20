f76e0fb26c5f3ee5029be498e22a0deeec4b6690
pranjal <pranjalg2501@gmail.com>
2020-04-20 16:52:48 +0530

    stack using array

---
+++ stack/stack_arr.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice = 1, max;
+    scanf("%d", &max);
+    int array[max];
+    printf("Select choice");
+    printf("1) push");
+    printf("2) pop");
+    printf("3) peek");
+    printf("4) display");
+    printf("5) Exit");
+    scanf("%d", &choice);
+    while (choice)
+    {
+        printf("Select choice");
+        printf("1) push");
+        printf("2) pop");
+        printf("3) peek");
+        printf("4) display");
+        printf("5) Exit");
+
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        }
+        printf("Enter the choice");
+        scanf("%d", &choice);
+    }
+}
