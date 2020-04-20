0acf7e074cd1571d0513e4c5a8a5198af17aabdb
pranjal <pranjalg2501@gmail.com>
2020-04-20 17:22:35 +0530

    ds

---
+++ LinkList/sta.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Select choice");
+    printf("1) push");
+    printf("2) pop");
+    printf("3) peek");
+    printf("4) display");
+    printf("5) Exit");
+}
